4bfce23bc15ae0fe8a3db250c8dd27c59abd09f4
Tracy Chan <ss.static@gmail.com>
2022-12-22 13:07:48 +0900

    Add HTML to PDF generation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pdf = require('html-pdf');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,5 +8,40 @@ router.get('/', function(req, res, next) {
   const { transactions, totalFiatDeposits, totalCryptoDeposits, totalInterestEarned, totalInterestEarnedInAud, totalFiatWithdrawals, totalCryptoWithdrawals, totalGainLoss, transactionHistory } = data;
   res.render('invoice', { transactions, totalFiatDeposits, totalCryptoDeposits, totalInterestEarned, totalInterestEarnedInAud, totalFiatWithdrawals, totalCryptoWithdrawals, totalGainLoss, transactionHistory });
 });
+
+router.get('/pdf', async (req, res) => {
+  const data = require('../response.json');
+  const { transactions, totalFiatDeposits, totalCryptoDeposits, totalInterestEarned, totalInterestEarnedInAud, totalFiatWithdrawals, totalCryptoWithdrawals, totalGainLoss, transactionHistory } = data;
+  res.render(
+    'invoice', { transactions, totalFiatDeposits, totalCryptoDeposits, totalInterestEarned, totalInterestEarnedInAud, totalFiatWithdrawals, totalCryptoWithdrawals, totalGainLoss, transactionHistory },
+    function(error, html) {
+      var config = {
+        format: "A4",
+        orientation: "portrait",
+        timeout: 100000,
+        phantomArgs: ["--web-security=false","--local-to-remote-url-access=true"]
+      };
+      pdf.create(html, config).toStream((err, pdfStream) => {
+        if (err) {
+          // handle error and return a error response code
+          console.log(err)
+          return res.sendStatus(500)
+        } else {
+          // send a status code of 200 OK
+          res.statusCode = 200
+
+          // once we are done reading end the response
+          pdfStream.on('end', () => {
+            // done reading
+            return res.end()
+          })
+
+          // pipe the contents of the PDF directly to the response
+          pdfStream.pipe(res)
+        }
+      })
+    },
+  );
+})
 
 module.exports = router;
