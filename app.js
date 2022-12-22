c950fbadcad41177f2450aa7a40997506ab91630
Tracy Chan <ss.static@gmail.com>
2022-12-22 12:06:43 +0900

    Add mustache engine for express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var mustacheExpress = require('mustache-express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -9,9 +10,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'mustache');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,14 +32,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
