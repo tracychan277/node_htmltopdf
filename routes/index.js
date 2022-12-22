0c3271b0d1344c5a08eebf30b8e7203b5d2ddbcb
Tracy Chan <ss.static@gmail.com>
2022-12-22 12:17:53 +0900

    Inject json data into template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('invoice', { title: 'Express' });
+  const data = require('../response.json');
+  const { transactions, totalFiatDeposits, totalCryptoDeposits, totalInterestEarned, totalInterestEarnedInAud, totalFiatWithdrawals, totalCryptoWithdrawals, totalGainLoss, transactionHistory } = data;
+  res.render('invoice', { transactions, totalFiatDeposits, totalCryptoDeposits, totalInterestEarned, totalInterestEarnedInAud, totalFiatWithdrawals, totalCryptoWithdrawals, totalGainLoss, transactionHistory });
 });
 
 module.exports = router;
